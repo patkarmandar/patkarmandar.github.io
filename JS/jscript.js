cced26689464f38b5c9f1571043217cd279d45ea
patkarmandar <87449373+patkarmandar@users.noreply.github.com>
2021-07-15 03:15:02 +0530

    updated repo (old website)

---
+++ JS/jscript.js
@@ -0,0 +1,8 @@
+/* Navigation */
+function openNav() {
+	document.getElementById("idSidenav").style.display = "block";
+}
+
+function closeNav() {
+	document.getElementById("idSidenav").style.display = "none";
+}
